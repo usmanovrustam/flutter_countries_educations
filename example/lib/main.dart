f0f9e18e68c8e61695d7b93d5d89ab2af401fbb8
Rustam Usmanov <rustamusmanov708@gmail.com>
2024-05-29 13:27:26 +0200

    flag image state added to the release

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,7 +35,6 @@ class _MyAppState extends State<MyApp> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              FlagImage.getImage(),
               const Text("Address"),
               AddressSelector.country(
                 title: 'Select Country',
--- lib/presentation/flag_image.dart
+++ lib/presentation/flag_image.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_countries_educations/src/assets/assets.gen.dart';
 
 class FlagImage {
-  static Widget getImage({String countryCode = "UZ"}) {
-    return SvgGenImage(
-      "assets/images/flags/${countryCode.toLowerCase()}.svg",
-    ).svg(fit: BoxFit.none);
+  static Widget getImage({required String countryCode}) {
+    final path = 'assets/images/flags/${countryCode.toLowerCase()}.svg';
+
+    return SvgGenImage(path).svg(
+      fit: BoxFit.cover,
+      placeholderBuilder: (context) => const Icon(Icons.error),
+    );
   }
 }
