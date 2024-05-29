5faa5491cfd29e5619843a4ceaeae00cb043a2a9
Rustam Usmanov <rustamusmanov708@gmail.com>
2024-05-29 13:00:43 +0200

    Constants file populated to the release

--- lib/flutter_countries_educations.dart
+++ lib/flutter_countries_educations.dart
@@ -2,3 +2,4 @@ export 'package:flutter_countries_educations/data/model/address/address.dart';
 export 'package:flutter_countries_educations/data/model/education/education.dart';
 export 'package:flutter_countries_educations/presentation/address_selector.dart';
 export 'package:flutter_countries_educations/presentation/education_selector.dart';
+export 'package:flutter_countries_educations/src/constants.dart';
